fc942173a9ae4170e5303ab4ecdab1e7b2ddbb70
ow294 <ow294@bath.ac.uk>
2021-08-07 12:33:21 +0100

    Moving chromedriver

--- constants.py
+++ constants.py
@@ -14,7 +14,7 @@ GOOGLE_HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55'
 }
 
-WEBDRIVER_PATH = "other\chromedriver.exe"
+WEBDRIVER_PATH = "other\chromedriver\chromedriver.exe"
 
 # Sentiment Analysis
 
