e072ed788bf937b45e3259ee5c46d70605ea0ab6
ow294 <ow294@bath.ac.uk>
2021-08-24 10:47:38 +0100

    Top 50 stocks folder and Experiment 3

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ def snp_stocks_full():
 
 def experiment_1():
     experiment_values = [(0.001, 0.99), (0.001, 0.9), (0.001, 0.7), (0.001, 0), (0.0001, 0), (0.0005, 0), (0.005, 0), (0.01, 0)]
+    experiment_values = [(0.0003, 0), (0.0004, 0), (0.0006, 0), (0.0007, 0)]
     repeats = 10
     total_training_steps = 100_000
     attributes = ['cheats']
@@ -143,6 +144,71 @@ def experiment_2():
         training_results.to_excel(path + f"{seperator.join(attributes)}_training.xlsx")
         testing_results.to_excel(path + f"{seperator.join(attributes)}_testing.xlsx")
 
+def experiment_3():
+    test_attributes = [
+        ['hf_google_articles_score'],
+        ['ranking_change_score', 'ranking_score', 'hf_google_articles_score'],
+        ['macd', 'signal_line', 'normalized_rsi', 'std_devs_out', 'relative_vol', 'ranking_change_score', 'ranking_score', 'hf_google_articles_score']   
+    ]
+    test_attributes = []
+
+    repeats = 10
+    total_training_steps = 150_000
+    alpha = 0.0005
+    gamma = 0
+    stocks = retrieve_stocks_from_folder("data/snp_50_stocks_full")
+
+    train_dfs = [s.df.loc[100:1000] for s in stocks[:]]
+    train_episodes = total_training_steps // (len(train_dfs[0]) - 1)
+    test_dfs = [s.df.loc[1000:] for s in stocks[:]]
+
+    # Test Baseline
+    testing_results = pd.DataFrame({"Episode": [1]})
+    for i in range(repeats):
+        train_env = PortfolioAllocationEnvironment(train_dfs, ['macd'])
+        model = A2C('MlpPolicy', train_env, verbose=0, learning_rate=alpha, gamma=gamma)
+        test_env = PortfolioAllocationEnvironment(test_dfs, ['macd'])
+        obs = test_env.reset()
+        while True:
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, info = test_env.step(action)
+            if done:
+                break
+        testing_results[i + 1] = [test_env.portfolio_value]
+    path = f"data/results/experiment_3/"
+    seperator = "-"        
+    testing_results.to_excel(path + f"baseline_testing.xlsx")
+        
+
+    # Agents
+    for attributes in test_attributes:
+        print(f"Attributes = {attributes}")
+        training_results = pd.DataFrame({"Episode": [(i + 1) for i in range(train_episodes)]})
+        testing_results = pd.DataFrame({"Episode": [1]})
+        for i in range(repeats):
+            print(f"Repeat {i + 1}")
+            train_env = PortfolioAllocationEnvironment(train_dfs, attributes)
+            train_env.reset()
+            model = A2C('MlpPolicy', train_env, verbose=0, learning_rate=alpha, gamma=gamma)
+            model.learn(total_timesteps=total_training_steps)
+
+            training_results[i + 1] = train_env.final_values
+
+            test_env = PortfolioAllocationEnvironment(test_dfs, attributes)
+            obs = test_env.reset()
+            while True:
+                action, _state = model.predict(obs, deterministic=True)
+                obs, reward, done, info = test_env.step(action)
+                if done:
+                    break
+
+            testing_results[i + 1] = [test_env.portfolio_value]
+        
+        path = f"data/results/experiment_3/"
+        seperator = "-"        
+        training_results.to_excel(path + f"{seperator.join(attributes)}_training.xlsx")
+        testing_results.to_excel(path + f"{seperator.join(attributes)}_testing.xlsx")
+
 def figs(experiment_name, folder):
     training_dict = {}
     testing_dict = {}
@@ -223,6 +289,19 @@ if __name__ == "__main__":
     # # experiment_2()
     # figs("E1_1", "data/results/e_1_1")
     # figs("E1", "data/results/experiment_1")
+
+
+    # stocks = retrieve_stocks_from_folder("data/snp_stocks_full")
+    # print(stocks[0].df.columns)
+    # stocks_50 = [s for s in stocks if s.code in [sa[1] for sa in SNP_500_TOP_100[:50]]]
+    # for s in stocks_50:
+    #     print(s.code)
+    #     save_stock(s, "data/snp_50_stocks_full")
+
+
+    # print(len(stocks_50))
+    # for s in stocks:
+    #     print(s.code)
 
 # OLD FUNCTIONS
 
