10d7c61c32aa42d007adeafc6d4f13faf3ea5ce2
ow294 <ow294@bath.ac.uk>
2021-08-26 16:03:56 +0100

    Combinded features refined implementation

--- main.py
+++ main.py
@@ -437,6 +437,68 @@ def e4_combined_features():
         testing_results.to_excel(path + f"{seperator.join(attributes)}_testing.xlsx")
 
 
+def e4_2_combined_features_refined():
+    path = "data/results/raw_combined_features_refined/"
+
+    test_attributes = [
+        ['normalized_rsi', 'std_devs_out', 'relative_vol'],
+        ['hf_google_articles_score', 'vader_google_articles_score', 'ranking_score'],
+        ['normalized_rsi', 'std_devs_out', 'relative_vol',
+            'hf_google_articles_score', 'vader_google_articles_score', 'ranking_score']
+    ]
+    repeats = 10
+    total_training_steps = 200_000
+    alpha = 0.0005
+    gamma = 0
+    stocks = retrieve_stocks_from_folder("data/snp_50_stocks_full_updated")
+
+    train_dfs = [s.df.loc[100:1000] for s in stocks[:]]
+    train_episodes = total_training_steps // (len(train_dfs[0]) - 1)
+    test_dfs = [s.df.loc[1000:] for s in stocks[:]]
+
+    # Test Baseline
+    testing_results = pd.DataFrame({"Episode": [1]})
+    for i in range(repeats):
+        train_env = PortfolioAllocationEnvironment(train_dfs, ['macd'])
+        model = A2C('MlpPolicy', train_env, verbose=0, learning_rate=alpha, gamma=gamma)
+        test_env = PortfolioAllocationEnvironment(test_dfs, ['macd'])
+        obs = test_env.reset()
+        while True:
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, info = test_env.step(action)
+            if done:
+                break
+        testing_results[i + 1] = [test_env.portfolio_value]
+    testing_results.to_excel(path + f"untrained_testing.xlsx")
+        
+    # Agents
+    for attributes in test_attributes:
+        print(f"Attributes = {attributes}")
+        training_results = pd.DataFrame({"Episode": [(i + 1) for i in range(train_episodes)]})
+        testing_results = pd.DataFrame({"Episode": [1]})
+        for i in range(repeats):
+            print(f"Repeat {i + 1}")
+            train_env = PortfolioAllocationEnvironment(train_dfs, attributes)
+            train_env.reset()
+            model = A2C('MlpPolicy', train_env, verbose=0, learning_rate=alpha, gamma=gamma)
+            model.learn(total_timesteps=total_training_steps)
+
+            training_results[i + 1] = train_env.final_values
+
+            test_env = PortfolioAllocationEnvironment(test_dfs, attributes)
+            obs = test_env.reset()
+            while True:
+                action, _state = model.predict(obs, deterministic=True)
+                obs, reward, done, info = test_env.step(action)
+                if done:
+                    break
+
+            testing_results[i + 1] = [test_env.portfolio_value]
+        
+        seperator = "-"        
+        training_results.to_excel(path + f"{seperator.join(attributes)}_training.xlsx")
+        testing_results.to_excel(path + f"{seperator.join(attributes)}_testing.xlsx")
+
 def plots_and_stats(experiment_name, name_of_independent, folder, log_scale=True):    
     training_dict = {}
     testing_dict = {}
@@ -521,12 +583,13 @@ if __name__ == "__main__":
     # experiment_3()
     # e3_sentiment_features()
     # e4_combined_features()
+    e4_2_combined_features_refined()
     # plots_and_stats("Learning Rate Narrow Search", "Learning Rate", "data/results/learning_rate_narrow_search")
     # plots_and_stats("Learning Rate Broad Search", "Learning Rate", "data/results/learning_rate_broad_search")
     # plots_and_stats("Gamma Broad Search", "Gamma", "data/results/gamma_broad_search")
     # plots_and_stats("Technical Indicators Comparison", "Indicator", "data/results/technical_indicators", log_scale=False)
     # plots_and_stats("Raw sent", "Feature", "data/results/raw_sentiment_features", log_scale=False)
-    plots_and_stats("Combined Features", "Feature", "data/results/combined_features", log_scale=False)
+    # plots_and_stats("Combined Features", "Feature", "data/results/combined_features", log_scale=False)
 
 
     # stocks = retrieve_stocks_from_folder("data/snp_stocks_full")
