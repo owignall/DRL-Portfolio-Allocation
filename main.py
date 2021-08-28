40e3b04cbec4d1b2b2b4cd651b0f5b64249e9d62
ow294 <ow294@bath.ac.uk>
2021-08-28 15:41:10 +0100

    Experiment 5 Implementation

--- main.py
+++ main.py
@@ -198,7 +198,6 @@ def e1_parameter_search():
         (0.001, 0.99), (0.001, 0.9), (0.001, 0.7), (0.001, 0), (0.0001, 0), 
         (0.0005, 0), (0.005, 0), (0.01, 0), (0.0003, 0), (0.0004, 0), (0.0006, 0), (0.0007, 0)
     ]
-    experiment_values = [] # TEMP
     repeats = 10
     total_training_steps = 100_000
     attributes = ['cheats']
@@ -499,7 +498,102 @@ def e4_2_combined_features_refined():
         testing_results.to_excel(path + f"{seperator.join(attributes)}_testing.xlsx")
 
 def e5_model_comparison():
-    pass
+    path = "data/results/raw_model_comparison/"
+
+    attributes = ['normalized_rsi', 'std_devs_out', 'relative_vol', 
+        'hf_google_articles_score', 'vader_google_articles_score', 'ranking_score']
+    repeats = 10
+    total_training_steps = 200_000
+    stocks = retrieve_stocks_from_folder("data/snp_50_stocks_full_updated")
+
+    train_dfs = [s.df.loc[100:1000] for s in stocks[:]]
+    train_episodes = total_training_steps // (len(train_dfs[0]) - 1)
+    test_dfs = [s.df.loc[1000:] for s in stocks[:]]
+
+    # Agents
+    # DQN Untrained
+    testing_results = pd.DataFrame({"Values": ["Final Value", "Annualized Return", "Sharpe Ratio"]})
+    for i in range(repeats):
+        train_env = PortfolioAllocationEnvironment(train_dfs, attributes, discrete=True)
+        train_env.reset()
+        model = DQN('MlpPolicy', train_env, verbose=0, learning_rate=0.0001, gamma=0)
+        test_env = PortfolioAllocationEnvironment(train_dfs, attributes, discrete=True)
+        obs = test_env.reset()
+        while True:
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, info = test_env.step(action)
+            if done:
+                break
+        testing_results[i + 1] = [test_env.portfolio_value, test_env.annualized_return, test_env.sharpe_ratio]
+    testing_results.to_excel(path + f"DQN Untrained_testing.xlsx")
+    
+    # DQN
+    training_results = pd.DataFrame({"Episode": [(i + 1) for i in range(train_episodes)]})
+    testing_results = pd.DataFrame({"Values": ["Final Value", "Annualized Return", "Sharpe Ratio"]})
+    for i in range(repeats):
+        print(f"Repeat {i + 1}")
+        train_env = PortfolioAllocationEnvironment(train_dfs, attributes, discrete=True)
+        train_env.reset()
+        model = DQN('MlpPolicy', train_env, verbose=0, learning_rate=0.0001, gamma=0)
+        model.learn(total_timesteps=total_training_steps)
+
+        training_results[i + 1] = train_env.final_values
+
+        test_env = PortfolioAllocationEnvironment(test_dfs, attributes, discrete=True)
+        obs = test_env.reset()
+        while True:
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, info = test_env.step(action)
+            if done:
+                break
+
+        testing_results[i + 1] = [test_env.portfolio_value, test_env.annualized_return, test_env.sharpe_ratio]
+    seperator = "-"        
+    training_results.to_excel(path + f"DQN_training.xlsx")
+    testing_results.to_excel(path + f"DQN_testing.xlsx")
+
+    # A2C Untrained
+    testing_results = pd.DataFrame({"Values": ["Final Value", "Annualized Return", "Sharpe Ratio"]})
+    for i in range(repeats):
+        train_env = PortfolioAllocationEnvironment(train_dfs, attributes)
+        model = A2C('MlpPolicy', train_env, verbose=0, learning_rate=0.0005, gamma=0)
+        test_env = PortfolioAllocationEnvironment(test_dfs, attributes)
+        obs = test_env.reset()
+        while True:
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, info = test_env.step(action)
+            if done:
+                break
+        testing_results[i + 1] = [test_env.portfolio_value, test_env.annualized_return, test_env.sharpe_ratio]
+    testing_results.to_excel(path + f"A2C Untrained_testing.xlsx")
+    
+    # A2C
+    training_results = pd.DataFrame({"Episode": [(i + 1) for i in range(train_episodes)]})
+    testing_results = pd.DataFrame({"Values": ["Final Value", "Annualized Return", "Sharpe Ratio"]})
+    for i in range(repeats):
+        print(f"Repeat {i + 1}")
+        train_env = PortfolioAllocationEnvironment(train_dfs, attributes)
+        train_env.reset()
+        model = A2C('MlpPolicy', train_env, verbose=0, learning_rate=0.0005, gamma=0)
+        model.learn(total_timesteps=total_training_steps)
+
+        training_results[i + 1] = train_env.final_values
+
+        test_env = PortfolioAllocationEnvironment(test_dfs, attributes)
+        obs = test_env.reset()
+        while True:
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, info = test_env.step(action)
+            if done:
+                break
+        testing_results[i + 1] = [test_env.portfolio_value, test_env.annualized_return, test_env.sharpe_ratio]
+    seperator = "-"        
+    training_results.to_excel(path + f"A2C_training.xlsx")
+    testing_results.to_excel(path + f"A2C_testing.xlsx")
+
+    
+
+    
 
 def plots_and_stats(experiment_name, name_of_independent, folder, log_scale=True):    
     training_dict = {}
